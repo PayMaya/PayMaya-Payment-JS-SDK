e543763970d387621f736df8927b9c2abc5b6b62
admin <mnfernandez@voyagerinnovation.com>
2015-12-04 18:46:12 +0800

    remove comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,16 +13,6 @@ module.exports = function(grunt) {
 			sdkAssetsPath : 'assets/sdk/'
 		},
 
-		/*concat : {
-			options : {
-				separator : ';'
-			},
-			dist : {
-				src : ['<%= meta.sdkAssetsPath %>paymaya.js'],
-				dest : 'dist/sdk/paymaya-sdk.js'
-			}
-		},*/
-
 		uglify : {
 			dist : {
 				files : {
@@ -61,11 +51,8 @@ module.exports = function(grunt) {
 
 	});
 
-	//grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-	//grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+	grunt.loadNpmTasks('grunt-contrib-cssmin')
 	grunt.registerTask('default', ['uglify', 'cssmin']);
 };
