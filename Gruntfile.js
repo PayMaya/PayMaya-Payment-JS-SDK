404fc67cd7269f03d7a8fa675bdd5931f51c295f
admin <mnfernandez@voyagerinnovation.com>
2015-12-05 03:55:00 +0800

    add cache buster / versioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,57 +2,73 @@
 
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		pkg : grunt.file.readJSON('package.json'),
+  grunt.initConfig({
 
-		// Metadata.
-		meta : {
-			cssDistPath : 'dist/css/',
-			sdkDistPath : 'dist/sdk/',
-			cssAssetsPath : 'assets/css/',
-			sdkAssetsPath : 'assets/sdk/'
-		},
+    pkg: grunt.file.readJSON('package.json'),
 
-		uglify : {
-			dist : {
-				files : {
-					'<%= meta.sdkDistPath %>paymaya-sdk.min.js' : ['<%= meta.sdkAssetsPath %>paymaya-sdk.js']
-				}
-			}
-		},
+    meta: {
+      cssDistPath: 'dist/css/',
+      sdkDistPath: 'dist/sdk/',
+      cssAssetsPath: 'assets/css/',
+      sdkAssetsPath: 'assets/sdk/'
+    },
 
-		cssmin : {
-			target : {
-				files : [{
-					expand : true,
-					cwd : '<%= meta.cssAssetsPath %>',
-					src : ['*.css', '!*.min.css'],
-					dest : '<%= meta.cssDistPath %>',
-					ext : '.min.css'
-				}]
-			}
-		},
-		watch : {
-			dev : {
-				files : ['Gruntfile.js', '<%= meta.sdkAssetsPath %>*.js', '<%= meta.sdkDistPath %>*.js', '*.html'],
-				tasks : ['uglify:dist'],
-				options : {
-					atBegin : true
-				}
-			},
-			min : {
-				files : ['Gruntfile.js', '<%= meta.sdkAssetsPath %>*.js', '<%= meta.sdkDistPath %>*.js', '*.html'],
-				tasks : ['uglify:dist'],
-				options : {
-					atBegin : true
-				}
-			}
-		},
+    uglify: {
+      dist: {
+        files: {
+          '<%= meta.sdkDistPath %>paymaya-sdk.min.js': ['<%= meta.sdkAssetsPath %>paymaya-sdk.js']
+        }
+      }
+    },
 
-	});
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: '<%= meta.cssAssetsPath %>',
+          src: ['*.css', '!*.min.css'],
+          dest: '<%= meta.cssDistPath %>',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      dev: {
+        files: ['Gruntfile.js', '<%= meta.sdkAssetsPath %>*.js', '<%= meta.sdkDistPath %>*.js', '*.html'],
+        tasks: ['uglify:dist'],
+        options: {
+          atBegin: true
+        }
+      },
+      min: {
+        files: ['Gruntfile.js', '<%= meta.sdkAssetsPath %>*.js', '<%= meta.sdkDistPath %>*.js', '*.html'],
+        tasks: ['uglify:dist'],
+        options: {
+          atBegin: true
+        }
+      }
+    },
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.registerTask('default', ['uglify', 'cssmin']);
+    asset_cachebuster: {
+      options: {
+      	buster: '0.1.0',
+        //buster: Date.now(),
+        ignore: [],
+        htmlExtension: 'html'
+      },
+      build: {
+        files: {
+          '<%= meta.sdkDistPath %>paymaya-sdk.min.js': ['<%= meta.sdkDistPath %>paymaya-sdk.min.js'],
+          '<%= meta.cssDistPath %>style.min.css': ['<%= meta.cssDistPath %>style.min.css'],
+          'index.html': ['index.html']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-asset-cachebuster');
+  grunt.registerTask('default', ['uglify', 'cssmin', 'asset_cachebuster']);
 };
