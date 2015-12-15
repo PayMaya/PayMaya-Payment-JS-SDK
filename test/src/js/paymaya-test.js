c8809e2931348e2092b042814481506ae364398f
admin <mnfernandez@voyagerinnovation.com>
2015-12-15 08:55:02 +0800

    unit test

--- test/src/js/paymaya-test.js
+++ test/src/js/paymaya-test.js
@@ -16,85 +16,83 @@ var cardExpiryYear = "2016";
 
 // var validation = "";
 // var paymentFormSubmitBtn = "";
-var success = function() {
-};
-var failure = function() {
-};
+var success = function() {};
+var failure = function() {};
 
 describe("PayMaya.Payments", function() {
 
-	describe("constructor", function() {
+  describe("constructor", function() {
 
-		it("should set a paymaya's public facing key (pfKey) if provided", function() {
+    it("should set a paymaya's public facing key (pfKey) if provided", function() {
 
-			// var payMayaPayments = new PayMaya.Payments("bee386662c04e4c6f872b5d4b13b7b87bd84ac68f3a95b8998298b32cd");
-			var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
+      // var payMayaPayments = new PayMaya.Payments("bee386662c04e4c6f872b5d4b13b7b87bd84ac68f3a95b8998298b32cd");
+      var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
 
-			// ASSERT
-			// assert.equal(payMayaPayments.pfKey, paymayaPublicKey);
-			assert.equal(payMayaPayments.pfKey, "bee386662c04e4c6f872b5d4b13b7b87bd84ac68f3a95b8998298b32cd");
+      // ASSERT
+      // assert.equal(payMayaPayments.pfKey, paymayaPublicKey);
+      assert.equal(payMayaPayments.pfKey, "bee386662c04e4c6f872b5d4b13b7b87bd84ac68f3a95b8998298b32cd");
 
-		});
+    });
 
-	});
+  });
 
-	describe("#createPaymentToken", function() {
+  describe("#createPaymentToken", function() {
 
-		it("should set payment form element id (#paymentForm)", function() {
+    it("should set payment form element id (#paymentForm)", function() {
 
-			var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
-			payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
+      var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
+      payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
 
-			// ASSERT
-			// assert.equal(payMayaPayments.paymentForm, paymentForm);
-			assert.equal(payMayaPayments.paymentForm, "#payment-form");
+      // ASSERT
+      // assert.equal(payMayaPayments.paymentForm, paymentForm);
+      assert.equal(payMayaPayments.paymentForm, "#payment-form");
 
-		});
+    });
 
-		it("should set credit card number", function() {
+    it("should set credit card number", function() {
 
-			var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
-			payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
+      var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
+      payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
 
-			// ASSERT
-			assert.equal(payMayaPayments.cardNumber, "4242424242424242");
-			// assert.equal(payMayaPayments.cardNumber, cardNumber);
+      // ASSERT
+      assert.equal(payMayaPayments.cardNumber, "4242424242424242");
+      // assert.equal(payMayaPayments.cardNumber, cardNumber);
 
-		});
+    });
 
-		it("should set credit card cvc", function() {
+    it("should set credit card cvc", function() {
 
-			var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
-			payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
+      var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
+      payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
 
-			// ASSERT
-			assert.equal(payMayaPayments.cardCvc, "123");
-			// assert.equal(payMayaPayments.cardCvc, cardCvc);
+      // ASSERT
+      assert.equal(payMayaPayments.cardCvc, "123");
+      // assert.equal(payMayaPayments.cardCvc, cardCvc);
 
-		});
+    });
 
-		it("should set credit card expiry month", function() {
+    it("should set credit card expiry month", function() {
 
-			var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
-			payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
+      var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
+      payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
 
-			// ASSERT
-			assert.equal(payMayaPayments.cardExpiryMonth, "12");
-			// assert.equal(payMayaPayments.cardExpiryMonth, cardExpiryMonth);
+      // ASSERT
+      assert.equal(payMayaPayments.cardExpiryMonth, "12");
+      // assert.equal(payMayaPayments.cardExpiryMonth, cardExpiryMonth);
 
-		});
+    });
 
-		it("should set credit card expiry year", function() {
+    it("should set credit card expiry year", function() {
 
-			var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
-			payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
+      var payMayaPayments = new PayMaya.Payments(paymayaPublicKey);
+      payMayaPayments.createPaymentToken(paymentForm, cardNumber, cardCvc, cardExpiryMonth, cardExpiryYear, success, failure);
 
-			// ASSERT
-			assert.equal(payMayaPayments.cardExpiryYear, "2016");
-			// assert.equal(payMayaPayments.cardExpiryYear, cardExpiryYear);
+      // ASSERT
+      assert.equal(payMayaPayments.cardExpiryYear, "2016");
+      // assert.equal(payMayaPayments.cardExpiryYear, cardExpiryYear);
 
-		});
+    });
 
-	});
+  });
 
 });
